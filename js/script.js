179bf40bc941dccc711a21cb766016e439a3cf35
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2022-11-07 00:12:49 -0300

    move scripts to own directory

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ const botaoRuby = document.getElementById('ruby');
 const mainIframe = document.getElementById('main_iframe');
 
 botaoInicio.addEventListener('click', () => {
-	mainIframe.src = "inicio.html";
+	mainIframe.src = "paginas/inicio.html";
 })
 
 botaoSobreMim.addEventListener('click', () => {
-	mainIframe.src = "sobre-mim.html";
+	mainIframe.src = "paginas/sobre-mim.html";
 })
 
 botaoRuby.addEventListener('click', () => {
-	mainIframe.src = "ruby.html";
+	mainIframe.src = "paginas/ruby.html";
 })
